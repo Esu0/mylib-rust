7c92633a9046997a1c3df4292cd0d4a429e57e96
Shintarou SHIMADA <s2413785@u.tsukuba.ac.jp>
2024-05-25 02:51:28 +0900

    optimize

--- persistent-array/src/stack.rs
+++ persistent-array/src/stack.rs
@@ -1,70 +1,54 @@
 use std::{
     alloc::{handle_alloc_error, Layout},
-    cell::Cell,
+    cell::{Cell, UnsafeCell},
     mem::MaybeUninit,
     ptr::NonNull,
 };
 
-pub struct PersistentStackPool<T: ?Sized> {
-    pool: Box<[Cell<MaybeUninit<Node<T>>>]>,
+pub struct PersistentStackPool<T> {
+    pool: Box<[UnsafeCell<MaybeUninit<Node<T>>>]>,
     len: Cell<usize>,
 }
 
-unsafe impl<T: ?Sized + Send> Send for PersistentStackPool<T> {}
+// unsafe impl<T: Send> Send for PersistentStackPool<T> {}
 
-impl<T: ?Sized> Drop for PersistentStackPool<T> {
+impl<T> Drop for PersistentStackPool<T> {
     fn drop(&mut self) {
         let len = self.len.get();
-        for node in &self.pool[..len] {
+        for node in &mut self.pool[..len] {
             unsafe {
-                node.get().assume_init().drop_value();
+                node.get_mut().assume_init_drop();
             }
         }
     }
 }
 
-pub struct PersistentStack<'a, T: ?Sized> {
+pub struct PersistentStack<'a, T> {
     head: usize,
     pool: &'a PersistentStackPool<T>,
 }
 
-impl<'a, T: ?Sized> Clone for PersistentStack<'a, T> {
+impl<'a, T> Clone for PersistentStack<'a, T> {
     fn clone(&self) -> Self {
         *self
     }
 }
 
-impl<'a, T: ?Sized> Copy for PersistentStack<'a, T> {}
+impl<'a, T> Copy for PersistentStack<'a, T> {}
 
-struct Node<T: ?Sized> {
+struct Node<T> {
     prev: usize,
-    value: NonNull<T>,
+    value: T,
 }
 
 impl<T> Node<T> {
     fn new(prev: usize, value: T) -> Self {
         Self {
             prev,
-            value: Box::leak(Box::new(value)).into(),
+            value,
         }
     }
 }
-
-impl<T: ?Sized> Node<T> {
-    fn drop_value(self) {
-        unsafe {
-            drop(Box::from_raw(self.value.as_ptr()))
-        }
-    }
-}
-
-impl<T: ?Sized> Clone for Node<T> {
-    fn clone(&self) -> Self {
-        *self
-    }
-}
-
-impl<T: ?Sized> Copy for Node<T> {}
 
 impl<T> PersistentStackPool<T> {
     pub fn new(size: usize) -> Self {
@@ -76,7 +60,7 @@ impl<T> PersistentStackPool<T> {
         }
         unsafe {
             let layout = Layout::array::<MaybeUninit<Node<T>>>(size).unwrap();
-            let ptr = std::alloc::alloc(layout) as *mut Cell<MaybeUninit<Node<T>>>;
+            let ptr = std::alloc::alloc(layout) as *mut _;
             let Some(ptr) = NonNull::new(ptr) else {
                 handle_alloc_error(layout);
             };
@@ -90,32 +74,33 @@ impl<T> PersistentStackPool<T> {
             }
         }
     }
-}
 
-impl<T: ?Sized> PersistentStackPool<T> {
     pub fn get_empty_stack(&self) -> PersistentStack<'_, T> {
         PersistentStack {
             head: usize::MAX,
             pool: self,
         }
     }
-
+    
     #[cfg(test)]
     fn check_invariant(&self) {
         let len = self.len.get();
         assert!(len <= self.pool.len());
         for i in 0..len {
-            let node = unsafe { self.pool[i].get().assume_init() };
-            assert!(node.prev == usize::MAX || node.prev < len);
+            let node_prev = unsafe { (*self.pool[i].get()).assume_init_ref().prev };
+            assert!(node_prev == usize::MAX || node_prev < len);
         }
     }
 }
+
 
 impl<'a, T> PersistentStack<'a, T> {
     pub fn push(&self, value: T) -> Self {
         let new_node = Node::new(self.head, value);
         let pool_last = self.pool.len.get();
-        self.pool.pool[pool_last].set(MaybeUninit::new(new_node));
+        unsafe {
+            self.pool.pool[pool_last].get().write(MaybeUninit::new(new_node));
+        }
         self.pool.len.set(pool_last + 1);
         Self {
             head: pool_last,
@@ -127,7 +112,7 @@ impl<'a, T> PersistentStack<'a, T> {
         if self.head == usize::MAX {
             None
         } else {
-            Some(unsafe { self.pool.pool[self.head].get().assume_init().value.as_ref() })
+            Some(unsafe { &(*self.pool.pool[self.head].get()).assume_init_ref().value })
         }
     }
 
@@ -136,7 +121,7 @@ impl<'a, T> PersistentStack<'a, T> {
             *self
         } else {
             Self {
-                head: unsafe { self.pool.pool[self.head].get().assume_init().prev },
+                head: unsafe { (*self.pool.pool[self.head].get()).assume_init_ref().prev },
                 pool: self.pool,
             }
         }
@@ -187,5 +172,22 @@ mod tests {
         assert_eq!(foo_bar.pop().top().unwrap(), "foo");
         assert_eq!(hello_world.top().unwrap(), "world");
         assert_eq!(hello_world.pop().top().unwrap(), "hello");
+    }
+
+    #[test]
+    #[should_panic]
+    fn out_of_bounds() {
+        let pool = PersistentStackPool::new(5);
+        let stack = pool.get_empty_stack();
+        let s = "hello".to_owned();
+        stack.push(s.clone()).push(s.clone()).push(s.clone()).push(s.clone()).push(s.clone()).push(s.clone());
+    }
+
+    #[test]
+    #[should_panic]
+    fn zero_size() {
+        let pool = PersistentStackPool::new(0);
+        let stack = pool.get_empty_stack();
+        stack.push(42);
     }
 }
