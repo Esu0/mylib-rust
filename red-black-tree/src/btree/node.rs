27adf921c5befdbb5bccb3874fce6423a875602d
Shintaro Shimada <87416336+Esu0@users.noreply.github.com>
2024-05-30 17:34:17 +0000

    until into_leaf impl

--- red-black-tree/src/btree/node.rs
+++ red-black-tree/src/btree/node.rs
@@ -1,4 +1,9 @@
-use std::{alloc::{self, handle_alloc_error, Layout}, marker::PhantomData, mem::{size_of, MaybeUninit}, ptr::{self, NonNull}};
+use std::{
+    alloc::{self, handle_alloc_error, Layout},
+    marker::PhantomData,
+    mem::{size_of, MaybeUninit},
+    ptr::{self, NonNull},
+};
 
 pub const CAPACITY: usize = 3;
 
@@ -56,23 +61,51 @@ pub struct InternalNode<K, V> {
 }
 
 pub struct NodeRef<BorrowType, K, V, Type> {
-    height: u8,
+    height: usize,
     node: NonNull<LeafNode<K, V>>,
     _marker: PhantomData<(BorrowType, Type)>,
 }
 
+pub struct Handle<Node, Type> {
+    node: Node,
+    idx: usize,
+    _marker: PhantomData<Type>,
+}
+
 pub type Root<K, V> = NodeRef<marker::Owned, K, V, marker::LeafOrInternal>;
+
+impl<'a, K: 'a, V: 'a, Type> Copy for NodeRef<marker::Immut<'a>, K, V, Type> {}
+impl<'a, K: 'a, V: 'a, Type> Clone for NodeRef<marker::Immut<'a>, K, V, Type> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+unsafe impl<BorrowType, K: Sync, V: Sync, Type> Sync for NodeRef<BorrowType, K, V, Type> {}
+unsafe impl<K: Sync, V: Sync, Type> Send for NodeRef<marker::Immut<'_>, K, V, Type> {}
+unsafe impl<K: Send, V: Send, Type> Send for NodeRef<marker::Mut<'_>, K, V, Type> {}
+unsafe impl<K: Send, V: Send, Type> Send for NodeRef<marker::ValMut<'_>, K, V, Type> {}
+unsafe impl<K: Send, V: Send, Type> Send for NodeRef<marker::Owned, K, V, Type> {}
+unsafe impl<K: Send, V: Send, Type> Send for NodeRef<marker::Dying, K, V, Type> {}
 
 impl<K, V> LeafNode<K, V> {
     /// 新しい`LeafNode`をインプレースに初期化する。
     unsafe fn init(this: *mut Self) {
         ptr::addr_of_mut!((*this).len).write(0);
     }
+
+    fn new() -> Box<Self> {
+        unsafe {
+            let mut leaf = new_uninit_box();
+            LeafNode::init(leaf.as_mut_ptr());
+            assume_init_box(leaf)
+        }
+    }
 }
 
 impl<K, V> InternalNode<K, V> {
     /// 新しく`Box<InternalNode<K, V>>`を作成する。
-    /// 
+    ///
     /// # Safety
     /// "internal node"の不変条件として、少なくとも一つの有効な辺を持つという
     /// ものがある。この関数はそのような辺のセットアップをしないことに注意する。
@@ -83,6 +116,159 @@ impl<K, V> InternalNode<K, V> {
     }
 }
 
+impl<K, V> NodeRef<marker::Owned, K, V, marker::Leaf> {
+    pub fn new_leaf() -> Self {
+        Self::from_new_leaf(LeafNode::new())
+    }
+
+    fn from_new_leaf(leaf: Box<LeafNode<K, V>>) -> Self {
+        NodeRef {
+            height: 0,
+            node: Box::leak(leaf).into(),
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<K, V> NodeRef<marker::Owned, K, V, marker::Internal> {
+    pub fn new_internal(child: Root<K, V>) -> Self {
+        let mut new_node = unsafe { InternalNode::new() };
+        new_node.edges[0].write(child.node);
+        unsafe { Self::from_new_internal(new_node, child.height + 1)}
+    }
+
+    /// # Safety
+    /// `height`は0より大きい
+    unsafe fn from_new_internal(internal: Box<InternalNode<K, V>>, height: usize) -> Self {
+        debug_assert!(height > 0);
+        let node = NonNull::from(Box::leak(internal)).cast();
+        NodeRef {
+            height,
+            node,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<BorrowType, K, V> NodeRef<BorrowType, K, V, marker::Internal> {
+    /// もともとは`NodeRef`の`parent`フィールドから親の`NodeRef`を取得するための
+    /// メソッド。現在の実装では`parent`フィールドは存在しないので、使う予定はない。
+    #[allow(dead_code)]
+    fn from_internal(node: NonNull<InternalNode<K, V>>, height: usize) -> Self {
+        debug_assert!(height > 0);
+        NodeRef {
+            height,
+            node: node.cast(),
+            _marker: PhantomData,
+        }
+    }
+
+    /// `internal node`のデータを外に公開する。
+    /// 
+    /// このノードへのほかの参照を無効化しないように、生ポインタで返す。
+    fn as_internal_ptr(this: &Self) -> *mut InternalNode<K, V> {
+        this.node.as_ptr() as *mut InternalNode<K, V>
+    }
+}
+
+impl<'a, K, V> NodeRef<marker::Mut<'a>, K, V, marker::Internal> {
+    /// `internal node`のデータの排他的な参照を返す。
+    fn as_internal_mut(&mut self) -> &mut InternalNode<K, V> {
+        unsafe { &mut *NodeRef::as_internal_ptr(self) }
+    }
+}
+
+impl<BorrowType, K, V, Type> NodeRef<BorrowType, K ,V, Type> {
+    /// ノードの長さを返す。これはキーと値のペアの数である。
+    /// 辺の数は`len() + 1`である。
+    /// この関数は安全ではあるが、unsafeなコードから生成された排他参照を無効化する
+    /// ことに注意する。
+    pub fn len(&self) -> usize {
+        unsafe { (*Self::as_leaf_ptr(self)).len as usize }
+    }
+
+    /// ノードの高さを返す。葉ノードの高さは0である。
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    /// 同じノードへの一時的な共有参照を返す。
+    pub fn reborrow(&self) -> NodeRef<marker::Immut<'_>, K, V, Type> {
+        NodeRef { height: self.height, node: self.node, _marker: PhantomData }
+    }
+
+    /// "leaf node"あるいは"internal node"のデータを外に公開する。
+    /// 
+    /// このノードへのほかの参照を無効化しないように、生ポインタで返す。
+    fn as_leaf_ptr(this: &Self) -> *mut LeafNode<K, V> {
+        this.node.as_ptr()
+    }
+
+    fn eq(&self, other: &Self) -> bool {
+        let Self { node, height, _marker } = self;
+        if node.eq(&other.node) {
+            debug_assert_eq!(*height, other.height);
+            true
+        } else {
+            false
+        }
+    }
+}
+
+impl<'a, K: 'a, V: 'a, Type> NodeRef<marker::Immut<'a>, K, V, Type> {
+    fn into_leaf(self) -> &'a LeafNode<K, V> {
+        unsafe { &*NodeRef::as_leaf_ptr(&self) }
+    }
+}
+
+impl<BorrowType: marker::BorrowType, K, V, Type> NodeRef<BorrowType, K, V, Type> {
+    pub fn first_edge(self) -> Handle<Self, marker::Edge> {
+        unsafe { Handle::new_edge(self, 0) }
+    }
+
+    pub fn last_edge(self) -> Handle<Self, marker::Edge> {
+        let len = self.len();
+        unsafe { Handle::new_edge(self, len) }
+    }
+
+    /// `self`の長さが0であるとき、panicすることに注意
+    pub fn first_kv(self) -> Handle<Self, marker::KV> {
+        let len = self.len();
+        assert!(len > 0);
+        unsafe { Handle::new_kv(self, 0) }
+    }
+    
+    /// `self`の長さが0であるとき、panicすることに注意
+    pub fn last_kv(self) -> Handle<Self, marker::KV> {
+        let len = self.len();
+        assert!(len > 0);
+        unsafe { Handle::new_kv(self, len - 1) }
+    }
+}
+
+impl<BorrowType, K, V, NodeType> Handle<NodeRef<BorrowType, K, V, NodeType>, marker::Edge> {
+    /// edgeに対する新しいhandleを作成する。
+    /// 呼び出し側は`idx <= node.len()`であることを保証しなければならない。
+    pub unsafe fn new_edge(node: NodeRef<BorrowType, K, V, NodeType>, idx: usize) -> Self {
+        debug_assert!(idx <= node.len());
+        Handle {
+            node,
+            idx,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<BorrowType, K, V, NodeType> Handle<NodeRef<BorrowType, K, V, NodeType>, marker::KV> {
+    pub unsafe fn new_kv(node: NodeRef<BorrowType, K, V, NodeType>, idx: usize) -> Self {
+        debug_assert!(idx < node.len());
+        Handle {
+            node,
+            idx,
+            _marker: PhantomData,
+        }
+    }
+}
 pub mod marker {
     use std::marker::PhantomData;
 
@@ -110,13 +296,13 @@ pub mod marker {
     impl<'a> BorrowType for Mut<'a> {}
     impl<'a> BorrowType for ValMut<'a> {}
 
-    pub enum Val {}
+    pub enum KV {}
     pub enum Edge {}
 }
 
 /// 初期化済みの要素とそれに続く一つの未初期化の要素で構成されるスライスに要素を挿入する。
 /// 挿入位置の後ろの要素は一つずつ後ろにずらされる。
-/// 
+///
 /// # Safety
 /// スライスが`idx`以上の要素をもつこと
 unsafe fn slice_insert<T>(slice: &mut [MaybeUninit<T>], idx: usize, val: T) {
@@ -131,7 +317,7 @@ unsafe fn slice_insert<T>(slice: &mut [MaybeUninit<T>], idx: usize, val: T) {
 
 /// 要素をスライスから削除し、その要素を返す。
 /// このとき、削除位置の後ろの要素は一つずつ前にずらされ、後ろの未初期化の要素が一つ増える。
-/// 
+///
 /// # Safety
 /// スライスが`idx`個以上の要素をもつこと
 unsafe fn slice_remove<T>(slice: &mut [MaybeUninit<T>], idx: usize) -> T {
@@ -144,7 +330,7 @@ unsafe fn slice_remove<T>(slice: &mut [MaybeUninit<T>], idx: usize) -> T {
 }
 
 /// スライスの要素を左に`distance`だけシフトする。
-/// 
+///
 /// # Safety
 /// スライスが`distance`個以上の要素をもつこと
 unsafe fn slice_shl<T>(slice: &mut [MaybeUninit<T>], distance: usize) {
@@ -153,7 +339,7 @@ unsafe fn slice_shl<T>(slice: &mut [MaybeUninit<T>], distance: usize) {
 }
 
 /// スライスの要素を右に`distance`だけシフトする。
-/// 
+///
 /// # Safety
 /// スライスが`distance`個以上の要素をもつこと
 unsafe fn slice_shr<T>(slice: &mut [MaybeUninit<T>], distance: usize) {
@@ -163,7 +349,7 @@ unsafe fn slice_shr<T>(slice: &mut [MaybeUninit<T>], distance: usize) {
 
 /// スライスの全ての初期化済みの要素をムーブする。
 /// `dst.copy_from_slice(src)`のような動作をするが、`T: Copy`を要求しない。
-/// 
+///
 /// # Panics
 /// 二つのスライスの長さが異なるとき
 fn move_to_slice<T>(src: &mut [MaybeUninit<T>], dst: &mut [MaybeUninit<T>]) {
@@ -179,7 +365,9 @@ fn new_uninit_box<T>() -> Box<MaybeUninit<T>> {
     } else {
         let layout = Layout::new::<MaybeUninit<T>>();
         unsafe {
-            NonNull::new(alloc::alloc(layout)).unwrap_or_else(|| handle_alloc_error(layout)).cast()
+            NonNull::new(alloc::alloc(layout))
+                .unwrap_or_else(|| handle_alloc_error(layout))
+                .cast()
         }
     };
     unsafe { Box::from_raw(ptr.as_ptr()) }
