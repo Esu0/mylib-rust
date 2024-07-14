1ee90b241ad445decf19a3392b1dd412691742c5
Esu0 <jshintaro1018@gmail.com>
2024-07-14 20:31:38 +0900

    suffix-array

--- suffix-array/src/lib.rs
+++ suffix-array/src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::Deref;
+use std::{mem::{ManuallyDrop, MaybeUninit}, ops::Deref};
 
 pub struct SuffixArray<'a, T> {
     array: Vec<&'a [T]>
@@ -12,16 +12,90 @@ impl<'a, T> Deref for SuffixArray<'a, T> {
     }
 }
 
-impl<'a, T> SuffixArray<'a, T> {
-    pub fn new_simple(array: &'a [T]) -> Self
-    where
-        T: Ord,
-    {
+fn vec_uninit<T>(n: usize) -> Vec<MaybeUninit<T>> {
+    let mut v = Vec::with_capacity(n);
+    unsafe {
+        v.set_len(n);
+    }
+    v
+}
+
+impl<'a, T: Ord> SuffixArray<'a, T> {
+    pub fn new_simple(array: &'a [T]) -> Self {
         let mut suffixes = (0..array.len()).map(|i| &array[i..]).collect::<Vec<_>>();
         suffixes.sort_unstable();
         Self {
             array: suffixes,
         }
+    }
+
+    fn make_bucket(arr: &'a [T]) -> (Vec<usize>, Vec<usize>) {
+        debug_assert!(!arr.is_empty());
+        let mut buf = (0..arr.len()).collect::<Vec<_>>();
+        buf.sort_unstable_by(|&i, &j| arr[i].cmp(&arr[j]));
+        let mut bucket = ManuallyDrop::new(vec_uninit(arr.len()));
+        let mut i = 0;
+        let mut prev_aj = &arr[buf[0]];
+        bucket[buf[0]].write(i);
+        for (k, &j) in buf.iter().enumerate().skip(1) {
+            if &arr[j] != prev_aj {
+                i = k;
+            }
+            bucket[j].write(i);
+            prev_aj = &arr[j];
+        }
+        let ptr = bucket.as_mut_ptr();
+        let len = bucket.len();
+        let capacity = bucket.capacity();
+        unsafe {
+            (Vec::from_raw_parts(ptr as _, len, capacity), buf)
+        }
+    }
+
+    pub fn new_sa_is(array: &'a [T]) -> Self {
+        if array.is_empty() {
+            return Self {
+                array: Vec::new(),
+            };
+        }
+        #[derive(Clone, Copy, PartialEq, Eq)]
+        enum SuffixType {
+            S,
+            L,
+            Lms,
+        }
+
+        let n = array.len();
+        let mut t = Vec::with_capacity(n);
+        for (i, w) in array.windows(2).enumerate() {
+            let [a, b] = w else {
+                unsafe {
+                    std::hint::unreachable_unchecked()
+                }
+            };
+            use std::cmp::Ordering::*;
+            match a.cmp(b) {
+                Less => while {
+                    t.push(SuffixType::S);
+                    t.len() <= i
+                } {},
+                Equal => (),
+                Greater => while {
+                    t.push(SuffixType::L);
+                    t.len() <= i
+                } {},
+            }
+        }
+        t.push(SuffixType::S);
+        let mut prev = t[0];
+        for ti in &mut t[1..n] {
+            if let [SuffixType::L, SuffixType::S] = [prev, *ti] {
+                *ti = SuffixType::Lms;
+            }
+            prev = *ti;
+        }
+        let (bucket, buf) = Self::make_bucket(array);
+        todo!()
     }
 }
 
