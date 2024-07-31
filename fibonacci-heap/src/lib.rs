6e400b6d4e8d7fe4e1ecfee94909384a17e83cb1
Esu0 <jshintaro1018@gmail.com>
2024-07-31 11:10:43 +0900

    a

--- fibonacci-heap/src/lib.rs
+++ fibonacci-heap/src/lib.rs
@@ -2,7 +2,8 @@ use std::num::NonZeroUsize;
 
 use node::{NodeArray, NodeRef};
 
-mod node;
+pub mod node;
+pub use node::IncreaseKeyError;
 
 #[derive(Debug)]
 pub struct FibonacciHeap<K, T> {
@@ -11,11 +12,18 @@ pub struct FibonacciHeap<K, T> {
     len: usize,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Cursor(NonZeroUsize);
 
 impl From<NodeRef> for Cursor {
     fn from(value: NodeRef) -> Self {
         Self(value.inner())
+    }
+}
+
+impl From<usize> for Cursor {
+    fn from(value: usize) -> Self {
+        Self(NonZeroUsize::new(value + 1).expect("overflowed in Cursor::from(usize)"))
     }
 }
 
@@ -52,6 +60,29 @@ impl<K, T> FibonacciHeap<K, T> {
     pub const fn is_empty(&self) -> bool {
         self.len == 0
     }
+
+    pub fn get_key(&self, cursor: Cursor) -> Option<&K> {
+        self.nodes.get_checked(cursor.0).map(|node| node.key())
+    }
+
+    pub fn get_value(&self, cursor: Cursor) -> Option<&T> {
+        self.nodes.get_checked(cursor.0).map(|node| node.value())
+    }
+
+    pub fn get_key_value(&self, cursor: Cursor) -> Option<(&K, &T)> {
+        self.nodes.get_checked(cursor.0).map(|node| node.key_value())
+    }
+
+    pub fn get_value_mut(&mut self, cursor: Cursor) -> Option<&mut T> {
+        self.nodes.get_checked_mut(cursor.0).map(|node| node.value_mut())
+    }
+
+    pub fn get_key_value_mut(&mut self, cursor: Cursor) -> Option<(&K, &mut T)> {
+        self.nodes.get_checked_mut(cursor.0).map(|node| {
+            let (k_ref, v_ref) = node.key_value_mut();
+            (&*k_ref, v_ref)
+        })
+    }
 }
 
 impl<K: Ord, T> FibonacciHeap<K, T> {
@@ -79,10 +110,37 @@ impl<K: Ord, T> FibonacciHeap<K, T> {
         self.max.map(|max| {
             let (new_max, k, v) = unsafe { self.nodes.remove_max(max) };
             self.max = new_max;
+            self.len -= 1;
             (k, v)
         })
     }
+
+    /// cursorが指すノードのキーをそれよりも大きいキー値`new_key`に更新する。
+    ///
+    /// 元のキー値よりも小さいキー値を指定した場合はヒープ構造が壊れる
+    pub fn increase_key(&mut self, cursor: Cursor, new_key: K) -> Result<K, K> {
+        if let Some(max) = self.max {
+            self.nodes.increase_key_force_checked(max, cursor.0, new_key).map(|(old_key, new_max)| {
+                self.max = Some(new_max);
+                old_key
+            })
+        } else {
+            Err(new_key)
+        }
+    }
+
+    pub fn increase_key_checked(&mut self, cursor: Cursor, new_key: K) -> IncreaseKeyResult<K> {
+        if let Some(max) = self.max {
+            let (old_key, new_max) = self.nodes.increase_key_checked(max, cursor.0, new_key)?;
+            self.max = Some(new_max);
+            Ok(old_key)
+        } else {
+            Err(IncreaseKeyError::NotFound(new_key))
+        }
+    }
 }
+
+pub type IncreaseKeyResult<K> = Result<K, IncreaseKeyError<K>>;
 
 impl<K, T> Default for FibonacciHeap<K, T> {
     fn default() -> Self {
@@ -119,7 +177,7 @@ mod tests {
     #[test]
     fn dijkstra() {
         use proconio::input;
-        use std::{cmp::Reverse, collections::BinaryHeap};
+        use std::cmp::Reverse;
         let s = "17 44
 1 2 104
 1 3 80
@@ -193,6 +251,114 @@ mod tests {
                     heap.push(Reverse(next_w), next);
                 }
             }
+        }
+        assert_eq!(
+            dist[1..],
+            [104, 80, 152, 128, 124, 120, 116, 168, 144, 124, 188, 164, 140, 136, 156, 288]
+        );
+    }
+
+    #[test]
+    fn increase_key() {
+        fn mpow(mut base: u32, mut exp: u64, modulo: u32) -> u32 {
+            let mut result = 1;
+            while exp > 0 {
+                if exp & 1 != 0 {
+                    result = (result as u64 * base as u64 % modulo as u64) as u32;
+                }
+                base = (base as u64 * base as u64 % modulo as u64) as u32;
+                exp >>= 1;
+            }
+            result
+        }
+
+        let mut heap = FibonacciHeap::new();
+        let cursors = (0usize..10).map(|i| heap.push(dbg!(mpow(7, i as u64, 41)), i)).collect::<Vec<_>>();
+        assert_eq!(heap.pop().unwrap().0, 38);
+        assert_eq!(heap.increase_key(cursors[0], 10), Ok(1));
+        assert_eq!(heap.increase_key(cursors[2], 20), Ok(8));
+        assert_eq!(heap.increase_key(cursors[6], 40), Ok(20));
+        assert_eq!(heap.pop().unwrap().0, 40);
+        assert_eq!(heap.pop().unwrap().0, 37);
+        assert_eq!(heap.increase_key(cursors[0], 25), Ok(10));
+    }
+
+    #[test]
+    fn dijkstra_fast() {
+        use proconio::input;
+        use std::cmp::Reverse;
+        let s = "17 44
+1 2 104
+1 3 80
+2 4 64
+2 5 60
+2 6 56
+2 7 52
+2 8 48
+3 4 72
+3 5 48
+3 6 44
+3 7 40
+3 8 36
+4 9 32
+4 10 28
+4 11 24
+5 9 40
+5 10 16
+5 11 12
+6 9 48
+6 10 24
+6 11 0
+7 9 56
+7 10 32
+7 11 8
+8 9 64
+8 10 40
+8 11 16
+9 12 48
+9 13 44
+9 14 40
+9 15 36
+10 12 56
+10 13 32
+10 14 28
+10 15 24
+11 12 64
+11 13 40
+11 14 16
+11 15 12
+12 16 0
+13 16 8
+14 16 16
+15 16 24
+16 17 132";
+        let source = proconio::source::line::LineSource::new(s.as_bytes());
+        input! {
+            from source,
+            n: usize,
+            m: usize,
+            uvb: [(usize, usize, u64); m],
+        }
+
+        let mut adj_list = vec![vec![]; n];
+        for &(u, v, b) in &uvb {
+            adj_list[u - 1].push((v - 1, b));
+        }
+
+        let mut heap = FibonacciHeap::new();
+        heap.push(Reverse(0u64), 0);
+        for i in 1..n {
+            heap.push(Reverse(u64::MAX), i);
+        }
+
+        let mut dist = vec![u64::MAX; n];
+        while let Some((Reverse(w), node)) = heap.pop() {
+            dist[node] = w;
+            for &(next, b) in &adj_list[node] {
+                let next_w = w + b;
+                let _ = heap.increase_key_checked(Cursor::from(next), Reverse(next_w));
+            }
+            eprintln!("{:#?}", heap);
         }
         assert_eq!(
             dist[1..],
--- fibonacci-heap/src/node.rs
+++ fibonacci-heap/src/node.rs
@@ -1,25 +1,28 @@
 use std::num::NonZeroUsize;
 
 #[derive(Debug)]
-pub struct NodeArray<K, T> {
+pub(super) struct NodeArray<K, T> {
     nodes: Vec<Option<Node<K, T>>>,
 }
 
 impl<K, T> NodeArray<K, T> {
-    pub const fn new() -> Self {
+    pub(super) const fn new() -> Self {
         Self { nodes: Vec::new() }
     }
 
-    pub fn get(&self, index: NodeRef) -> &Node<K, T> {
+    pub(super) fn get(&self, index: NodeRef) -> &Node<K, T> {
         unsafe {
             self.nodes
                 .get_unchecked(index.0.get())
+                // .get(index.0.get())
+                // .unwrap()
                 .as_ref()
                 .unwrap_unchecked()
+                // .unwrap()
         }
     }
 
-    pub fn get_mut(&mut self, index: NodeRef) -> &mut Node<K, T> {
+    pub(super) fn get_mut(&mut self, index: NodeRef) -> &mut Node<K, T> {
         unsafe {
             self.nodes
                 .get_unchecked_mut(index.0.get())
@@ -28,14 +31,14 @@ impl<K, T> NodeArray<K, T> {
         }
     }
 
-    pub unsafe fn destroy(&mut self, index: NodeRef) -> Node<K, T> {
+    pub(super) unsafe fn destroy(&mut self, index: NodeRef) -> Node<K, T> {
         self.nodes
             .get_unchecked_mut(index.0.get())
             .take()
             .unwrap_unchecked()
     }
 
-    pub fn push_new_node(&mut self, node: Node<K, T>) -> NodeRef {
+    pub(super) fn push_new_node(&mut self, node: Node<K, T>) -> NodeRef {
         if self.nodes.is_empty() {
             self.nodes.reserve(2);
             self.nodes.push(None);
@@ -48,7 +51,7 @@ impl<K, T> NodeArray<K, T> {
         }
     }
 
-    pub fn insert_new_node_right(&mut self, key: K, value: T, node: NodeRef) -> NodeRef {
+    pub(super) fn insert_new_node_right(&mut self, key: K, value: T, node: NodeRef) -> NodeRef {
         let right = self.get(node).right;
         let new_node = Node::new(key, value, node, right);
         let new_node_ref = self.push_new_node(new_node);
@@ -57,7 +60,7 @@ impl<K, T> NodeArray<K, T> {
         new_node_ref
     }
 
-    pub unsafe fn push_cyclic_node(&mut self, key: K, value: T) -> NodeRef {
+    pub(super) unsafe fn push_cyclic_node(&mut self, key: K, value: T) -> NodeRef {
         let new_node_ref;
         if self.nodes.is_empty() {
             new_node_ref = NodeRef(NonZeroUsize::new_unchecked(1));
@@ -71,26 +74,26 @@ impl<K, T> NodeArray<K, T> {
         new_node_ref
     }
 
-    /// parentのdegreeを更新しない
-    pub fn join_child(&mut self, parent: NodeRef, node: NodeRef) {
+    pub(super) fn join_child(&mut self, parent: NodeRef, node: NodeRef) {
         let parent_mut = self.get_mut(parent);
         if let Some(child) = parent_mut.child {
             self.insert_right(child, node);
-            self.get_mut(child).parent = Some(parent);
-            // self.get_mut(parent).degree += 1;
+            self.get_mut(parent).degree += 1;
         } else {
             parent_mut.child = Some(node);
+            parent_mut.degree = 1;
             self.link_cyclic(node);
         }
+        self.get_mut(node).parent = Some(parent);
     }
 
-    pub fn link_cyclic(&mut self, node: NodeRef) {
+    pub(super) fn link_cyclic(&mut self, node: NodeRef) {
         let node_mut = self.get_mut(node);
         node_mut.right = node;
         node_mut.left = node;
     }
 
-    pub fn insert_right(&mut self, position: NodeRef, node: NodeRef) {
+    pub(super) fn insert_right(&mut self, position: NodeRef, node: NodeRef) {
         let right = self.get(position).right;
         let node_mut = self.get_mut(node);
         node_mut.left = position;
@@ -98,10 +101,62 @@ impl<K, T> NodeArray<K, T> {
         self.get_mut(position).right = node;
         self.get_mut(right).left = node;
     }
+
+    pub(super) fn get_checked(&self, index: NonZeroUsize) -> Option<&Node<K, T>> {
+        self.nodes.get(index.get()).and_then(|node| node.as_ref())
+    }
+
+    pub(super) fn get_checked_mut(&mut self, index: NonZeroUsize) -> Option<&mut Node<K, T>> {
+        self.nodes.get_mut(index.get()).and_then(|node| node.as_mut())
+    }
+
+    fn get_node_ref(&self, index: NonZeroUsize) -> Option<(&Node<K, T>, NodeRef)> {
+        self.nodes.get(index.get()).and_then(|node| node.as_ref().map(|node| (node, NodeRef(index))))
+    }
+
+    fn cut_recursive(&mut self, node: NodeRef, parent: NodeRef) -> NodeRef {
+        let mut node = node;
+        let mut parent = parent;
+        // let mut node_list_tail = node;
+        let node_mut = self.get_mut(node);
+        let mut old_left = node_mut.left;
+        node_mut.parent = None;
+        node_mut.damaged = false;
+        while {
+            let old_right = self.get(node).right;
+
+            let parent_mut = self.get_mut(parent);
+            if old_left == node {
+                parent_mut.child = None;
+                parent_mut.degree = 0;
+            } else {
+                parent_mut.child = Some(old_left);
+                parent_mut.degree -= 1;
+                self.get_mut(old_left).right = old_right;
+                self.get_mut(old_right).left = old_left;
+            }
+            self.get(parent).damaged
+        } {
+            self.get_mut(node).right = parent;
+            let node_mut = self.get_mut(parent);
+            old_left = node_mut.left;
+            node_mut.left = node;
+            debug_assert!(node_mut.parent.is_some());
+            parent = unsafe { node_mut.parent.unwrap_unchecked() };
+            node_mut.parent = None;
+            node_mut.damaged = false;
+            node = parent;
+        }
+        let parent_mut = self.get_mut(parent);
+        if parent_mut.parent.is_some() {
+            parent_mut.damaged = true;
+        }
+        node
+    }
 }
 
 impl<K: Ord, T> NodeArray<K, T> {
-    pub unsafe fn remove_max(&mut self, max: NodeRef) -> (Option<NodeRef>, K, T) {
+    pub(super) unsafe fn remove_max(&mut self, max: NodeRef) -> (Option<NodeRef>, K, T) {
         let node = self.destroy(max);
         debug_assert!(node.parent.is_none());
         let mut buf = [Option::<NodeRef>::None; 64];
@@ -116,7 +171,7 @@ impl<K: Ord, T> NodeArray<K, T> {
             }
             max_degree = max_degree.max(degree);
             buf[degree as usize] = Some(tree);
-            self.get_mut(tree).degree = degree;
+            // self.get_mut(tree).degree = degree;
             tree = next;
         }
 
@@ -131,7 +186,7 @@ impl<K: Ord, T> NodeArray<K, T> {
                 }
                 max_degree = max_degree.max(degree);
                 buf[degree as usize] = Some(tree);
-                self.get_mut(tree).degree = degree;
+                // self.get_mut(tree).degree = degree;
                 tree = next;
                 tree != child
             } {}
@@ -161,8 +216,7 @@ impl<K: Ord, T> NodeArray<K, T> {
         )
     }
 
-    /// degreeを更新しない
-    pub fn merge_tree(&mut self, tree: NodeRef, other: NodeRef) -> NodeRef {
+    pub(super) fn merge_tree(&mut self, tree: NodeRef, other: NodeRef) -> NodeRef {
         let (min, max) = if self.get(tree).key < self.get(other).key {
             (tree, other)
         } else {
@@ -171,10 +225,62 @@ impl<K: Ord, T> NodeArray<K, T> {
         self.join_child(max, min);
         max
     }
+
+    pub(super) fn increase_key_force_checked(&mut self, max: NodeRef, node: NonZeroUsize, key: K) -> Result<(K, NodeRef), K> {
+        if let Some((node_ref, node)) = self.get_node_ref(node){
+            // let old_key = std::mem::replace(&mut node_ref.key, key);
+            let update_max = self.get(max).key < key;
+            let new_max = if update_max { node } else { max };
+            if let Some(parent) = node_ref.parent {
+                if update_max || self.get(parent).key < key {
+                    let node_list_tail = self.cut_recursive(node, parent);
+                    let root_mut = self.get_mut(max);
+                    let right = root_mut.right;
+                    root_mut.right = node;
+                    self.get_mut(node).left = max;
+                    self.get_mut(node_list_tail).right = right;
+                    self.get_mut(right).left = node_list_tail;
+                }
+            }
+            Ok((std::mem::replace(&mut self.get_mut(node).key, key), new_max))
+        } else {
+            Err(key)
+        }
+    }
+
+    pub(super) fn increase_key_checked(&mut self, max: NodeRef, node: NonZeroUsize, key: K) -> Result<(K, NodeRef), IncreaseKeyError<K>> {
+        if let Some((node_ref, node)) = self.get_node_ref(node){
+            // let old_key = std::mem::replace(&mut node_ref.key, key);
+            if node_ref.key > key {
+                return Err(IncreaseKeyError::LessKey(key));
+            }
+            let update_max = self.get(max).key < key;
+            let new_max = if update_max { node } else { max };
+            if let Some(parent) = node_ref.parent {
+                if update_max || self.get(parent).key < key {
+                    let node_list_tail = self.cut_recursive(node, parent);
+                    let root_mut = self.get_mut(max);
+                    let right = root_mut.right;
+                    root_mut.right = node;
+                    self.get_mut(node).left = max;
+                    self.get_mut(node_list_tail).right = right;
+                    self.get_mut(right).left = node_list_tail;
+                }
+            }
+            Ok((std::mem::replace(&mut self.get_mut(node).key, key), new_max))
+        } else {
+            Err(IncreaseKeyError::NotFound(key))
+        }
+    }
+}
+
+pub enum IncreaseKeyError<K> {
+    NotFound(K),
+    LessKey(K),
 }
 
 #[derive(Debug)]
-pub struct Node<K, T> {
+pub(super) struct Node<K, T> {
     key: K,
     value: T,
     degree: u16,
@@ -186,7 +292,7 @@ pub struct Node<K, T> {
 }
 
 impl<K, T> Node<K, T> {
-    pub fn new(key: K, value: T, left: NodeRef, right: NodeRef) -> Self {
+    pub(super) fn new(key: K, value: T, left: NodeRef, right: NodeRef) -> Self {
         Self {
             key,
             value,
@@ -199,24 +305,32 @@ impl<K, T> Node<K, T> {
         }
     }
 
-    pub fn key_value(&self) -> (&K, &T) {
+    pub(super) fn key_value(&self) -> (&K, &T) {
         (&self.key, &self.value)
     }
 
-    pub fn key(&self) -> &K {
+    pub(super) fn key(&self) -> &K {
         &self.key
     }
 
-    pub fn value(&self) -> &T {
+    pub(super) fn value(&self) -> &T {
         &self.value
+    }
+
+    pub(super) fn value_mut(&mut self) -> &mut T {
+        &mut self.value
+    }
+
+    pub(super) fn key_value_mut(&mut self) -> (&mut K, &mut T) {
+        (&mut self.key, &mut self.value)
     }
 }
 /// 有効なノードを参照するインデックス
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct NodeRef(NonZeroUsize);
+pub(super) struct NodeRef(NonZeroUsize);
 
 impl NodeRef {
-    pub const fn inner(self) -> NonZeroUsize {
+    pub(super) const fn inner(self) -> NonZeroUsize {
         self.0
     }
 }
